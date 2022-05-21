f8f20d8fdfac38af55f3f6d14c46bad7349d4611
Nathan Arthur <nathan@nathanarthur.com>
2022-05-21 17:29:56 -0400

    refactor

--- tiller/Code.networths.spec.ts
+++ tiller/Code.networths.spec.ts
@@ -3,10 +3,6 @@ import { NETWORTHS } from './Code';
 // Duplicated because Google Apps Script does not support ES6 modules
 const HEADER = ['Date', 'Assets', 'Liabilities', 'Net Worth', 'Delta'];
 
-function assertEquals(a: unknown, b: unknown): void {
-  expect(a).toEqual(b);
-}
-
 function assertNetWorths({
   assets,
   liabilities,
@@ -16,7 +12,7 @@ function assertNetWorths({
   liabilities?: LiabilityRow[];
   rows: NetWorthRow[];
 }): void {
-  assertEquals(NETWORTHS(assets || [], liabilities || []), [HEADER, ...rows]);
+  expect(NETWORTHS(assets || [], liabilities || [])).toEqual([HEADER, ...rows]);
 }
 
 describe('Tiller', () => {
@@ -76,7 +72,7 @@ describe('Tiller', () => {
   });
 
   it('handles empty row', () => {
-    assertEquals(NETWORTHS([['', '']], []), [HEADER]);
+    expect(NETWORTHS([['', '']], [])).toEqual([HEADER]);
   });
 
   it('passes legacy tests', () => {
@@ -91,20 +87,19 @@ describe('Tiller', () => {
         [new Date('3/11/22'), 1, 0, 1, 0]
       ]
     });
-    assertEquals(
+    expect(
       NETWORTHS(
         [
           [new Date('3/7/22'), 1],
           [new Date('3/11/22'), 2]
         ],
         []
-      ),
-      [
-        HEADER,
-        [new Date('3/7/22'), 1, 0, 1, 0],
-        [new Date('3/11/22'), 2, 0, 2, 1]
-      ]
-    );
+      )
+    ).toEqual([
+      HEADER,
+      [new Date('3/7/22'), 1, 0, 1, 0],
+      [new Date('3/11/22'), 2, 0, 2, 1]
+    ]);
     assertNetWorths({
       assets: [
         [new Date('3/7/22'), ''],
@@ -116,21 +111,20 @@ describe('Tiller', () => {
         [new Date('3/11/22'), 2, 0, 2, 0]
       ]
     });
-    assertEquals(
+    expect(
       NETWORTHS(
         [],
         [
           [new Date('3/7/22'), 1],
           [new Date('3/11/22'), 2]
         ]
-      ),
-      [
-        HEADER,
-        [new Date('3/7/22'), 0, -1, -1, 0],
-        [new Date('3/11/22'), 0, -2, -2, -1]
-      ]
-    );
-    assertEquals(
+      )
+    ).toEqual([
+      HEADER,
+      [new Date('3/7/22'), 0, -1, -1, 0],
+      [new Date('3/11/22'), 0, -2, -2, -1]
+    ]);
+    expect(
       NETWORTHS(
         [
           [new Date('3/7/22'), 1],
@@ -138,29 +132,27 @@ describe('Tiller', () => {
           [new Date('3/11/22'), 2]
         ],
         []
-      ),
-      [
-        HEADER,
-        [new Date('3/7/22'), 1, 0, 1, 0],
-        [new Date('3/8/22'), 1, 0, 1, 0],
-        [new Date('3/11/22'), 2, 0, 2, 1]
-      ]
-    );
-    assertEquals(
+      )
+    ).toEqual([
+      HEADER,
+      [new Date('3/7/22'), 1, 0, 1, 0],
+      [new Date('3/8/22'), 1, 0, 1, 0],
+      [new Date('3/11/22'), 2, 0, 2, 1]
+    ]);
+    expect(
       NETWORTHS(
         [
           [new Date('3/14/22'), '', 1726.93],
           [new Date('3/15/22'), 2008.86, '']
         ],
         [[''], ['']]
-      ),
-      [
-        HEADER,
-        [new Date('3/14/22'), 1726.93, 0, 1726.93, 0],
-        [new Date('3/15/22'), 3735.79, 0, 3735.79, 0]
-      ]
-    );
-    assertEquals(
+      )
+    ).toEqual([
+      HEADER,
+      [new Date('3/14/22'), 1726.93, 0, 1726.93, 0],
+      [new Date('3/15/22'), 3735.79, 0, 3735.79, 0]
+    ]);
+    expect(
       NETWORTHS(
         [
           [new Date('3/13/22'), 2008.86, 4121.93],
@@ -168,14 +160,13 @@ describe('Tiller', () => {
           [new Date('3/15/22'), 2008.86, '']
         ],
         [[''], ['']]
-      ),
-      [
-        HEADER,
-        [new Date('3/13/22'), 6130.79, 0, 6130.79, 0],
-        [new Date('3/14/22'), 3735.79, 0, 3735.79, -2395],
-        [new Date('3/15/22'), 3735.79, 0, 3735.79, 0]
-      ]
-    );
+      )
+    ).toEqual([
+      HEADER,
+      [new Date('3/13/22'), 6130.79, 0, 6130.79, 0],
+      [new Date('3/14/22'), 3735.79, 0, 3735.79, -2395],
+      [new Date('3/15/22'), 3735.79, 0, 3735.79, 0]
+    ]);
     assertNetWorths({
       assets: [
         [new Date('3/13/22'), 100, 0],
@@ -187,5 +178,89 @@ describe('Tiller', () => {
         [new Date('3/14/22'), 200, 0, 200, 100]
       ]
     });
+  });
+
+  it('calculates delta correctly', () => {
+    const assets: AssetRow[] = [
+      [
+        new Date('4/29/22'),
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        8107.39,
+        0.0,
+        5637.75,
+        459.85,
+        158.72
+      ],
+      [
+        new Date('4/30/22'),
+        '',
+        1941.8,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        2973.95,
+        3148.88,
+        8160.99,
+        0.0,
+        5685.24,
+        '',
+        158.72
+      ],
+      [
+        new Date('5/1/22'),
+        '',
+        1941.8,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        2973.95,
+        3148.88,
+        8062.84,
+        0.0,
+        '',
+        459.85,
+        158.72
+      ],
+      [
+        new Date('5/2/22'),
+        '',
+        1941.8,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        2973.95,
+        '',
+        8062.84,
+        0.0,
+        5603.53,
+        458.06,
+        ''
+      ]
+    ];
+    const result = NETWORTHS(assets, []);
+
+    expect(result).toEqual([
+      HEADER,
+      [new Date('4/29/22'), 14363.71, 0, 14363.71, 0],
+      [new Date('4/30/22'), 22529.43, 0, 22529.43, 101.08999999999924],
+      [new Date('5/1/22'), 22431.28, 0, 22431.28, -98.14999999999964],
+      [
+        new Date('5/2/22'),
+        22347.780000000002,
+        0,
+        22347.780000000002,
+        -83.50000000000006
+      ]
+    ]);
   });
 });
