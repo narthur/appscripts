0b21a3dd0264a8015d865b200a62e2112a8574ac
Nathan Arthur <nathan@nathanarthur.com>
2022-05-28 14:31:27 -0400

    fix public function declarations

--- tiller/src/index.ts
+++ tiller/src/index.ts
@@ -1,5 +1,16 @@
 import { LAST as _LAST } from './last';
 import { NETWORTHS as _NETWORTHS } from './networths';
 
-export const LAST = _LAST;
-export const NETWORTHS = _NETWORTHS;
+// Public functions need to be re-declared as functions in this file in order
+// for Rollup to include them in the output file, and for Google Sheets to
+// recognize them.
+
+export function LAST(...p: Parameters<typeof _LAST>): ReturnType<typeof _LAST> {
+  return _LAST(...p);
+}
+
+export function NETWORTHS(
+  ...p: Parameters<typeof _NETWORTHS>
+): ReturnType<typeof _NETWORTHS> {
+  return _NETWORTHS(...p);
+}
